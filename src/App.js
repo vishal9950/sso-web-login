c9924c4b1b98651c423998aec823f7b4925960f8
Vishal Vasnani <vishalvasnani123@gmail.com>
2019-02-15 12:10:51 +0530

    Added login page for sso poc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  onClick = () => {
+    console.log(document.cookie);
+    alert(document.cookie);
+  }
+
+  getCookie = (name) => {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
+  onClick1 = () => {
+    const token = this.getCookie('token');
+    console.log(token);
+    const options = {
+      method: 'GET',
+      url: '/restricted',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios(options).then((res) => {
+      const { message } = res.data;
+      alert(message);
+    }).catch(err => alert(err));
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,14 +39,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => this.onClick()}>Test set cookies</button>
+          <button onClick={() => this.onClick1()}>Test authorized response</button>
         </header>
       </div>
     );
